b8844a1b42c11fd61739e48c2bc4132f302ad972
Julian Hergoss <julian.hergoss@web.de>
2025-04-03 02:09:46 -0400

    Does vercel automatically deploy?

--- intersnhip/src/app/importer/page.tsx
+++ intersnhip/src/app/importer/page.tsx
@@ -4,7 +4,7 @@ import { transactionImporterData } from "@/app/importer/content/transaction-impo
 export default function Importer() {
   return (
     <main className="container mx-auto py-8 px-4 md:px-6">
-      <h1 className="text-3xl font-bold mb-8 text-center">Data Importer</h1>
+      <h1 className="text-3xl font-bold mb-8 text-center">Data Importer!</h1>
       <div className="space-y-12">
         <ProjectShowcase project={transactionImporterData} />
       </div>
